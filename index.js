d20ecd5a5b24a89349990e2f84888b607050dfb2
chris <chris.mahfouz@hotmail.com>
2021-03-16 11:02:59 +0200

    commit tree eb763fed564974e9adc010b51601d76a979f0055

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+
+var num1=31;
+var num2=2;
+
+var multiply=num1*num2;
+
+var random=Math.floor(Math.random())+1;
+
+
+var num3=18
+var num4=7
+
+var mod= num3%num4;
+
+var max=Math.max(1,2,3,4,5,20)
